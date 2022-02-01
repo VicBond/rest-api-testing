87033ab05b1e7646b6564afdf11a11a18e2320f3
Victor Bondarenko <nirasimha@gmail.com>
2022-02-01 13:24:33 -0500

    create a variable for books

--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+let books = [{
+  id: 1,
+  author: 'John Date',
+  title: 'JavaScript go go go'}];
+  
 router.get('/', (req, res) => {
-  res.json([{
-    id: 1,
-    author: 'John Date',
-    title: 'JavaScript go go go'
-  }])
+  res.json(books)
 });
 
 module.exports = router;
