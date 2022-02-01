5b791b48d8fda3879341e602f7eba33f6181d591
Victor Bondarenko <nirasimha@gmail.com>
2022-02-01 14:10:46 -0500

    install uuid & add post route

--- routes/books.js
+++ routes/books.js
@@ -4,10 +4,29 @@ const router = express.Router();
 let books = [{
   id: 1,
   author: 'John Date',
-  title: 'JavaScript go go go'}];
-  
+  title: 'JavaScript go go go'
+}];
+
 router.get('/', (req, res) => {
   res.json(books)
+});
+
+router.get('/:id', (req, res) => {
+  const bookId = parseInt(req.params.id, 10);
+  const book = books.find(book => book.id === bookId);
+  
+  if(book){
+    return res.json(book);
+  }
+  return res.status(404).json({ 
+    status: `Book with id ${bookId} not found`
+  });
+
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
 
 module.exports = router;
