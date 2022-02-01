be74194f113c0fa6e5bae12fb84b9fa4a680f8dd
Victor Bondarenko <nirasimha@gmail.com>
2022-02-01 11:19:55 -0500

    add listen on port in app

--- app.js
+++ app.js
@@ -17,4 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+// module.exports = app;
+app.listen(3000, () => {
+  console.log("It's started...", new Date());
+});
